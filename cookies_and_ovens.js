3bdd54387373cf9dfba1515a1c917d2b623a29bf
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-04 15:01:05 +0700

    simplify the cook process

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -25,6 +25,7 @@ class Cookies{
 class ChocolateCookies extends Cookies{
   constructor(status){
     super(status)
+    this.type = "Chocolate Cookies"
     this.timeToCook = 20;
   }
 }
@@ -32,98 +33,57 @@ class ChocolateCookies extends Cookies{
 class PeanutCookies extends Cookies{
   constructor(status){
     super(status)
+    this.type = "Peanut Cookies"
     this.timeToCook = 30;
   }
 }
 class CheeseCookies extends Cookies{
   constructor(status){
     super(status)
+    this.type = "Cheese Cookies"
     this.timeToCook = 35;
   }
 }
 
 class Oven {
   constructor(){
-    this.time=0;
-    this.cookedCookies=[];
-    this.discardedCookies=[];
+    this.inside=[];
   }
-  cookChocolateCookie(time){
-    this.time+=time
-    var chocolateCookies = new ChocolateCookies()
-    if (this.time >= chocolateCookies.timeToCook && this.time < chocolateCookies.timeToCook + 5){
-      chocolateCookies.status = 'cooked';
-      console.log(`Chocolate cookie is ${chocolateCookies.status} beautifully!`)
-      this.cookedCookies.push(chocolateCookies);
-      this.time=0;
-    }
-    if (this.time >= chocolateCookies.timeToCook - 5 && this.time < chocolateCookies.timeToCook){
-      chocolateCookies.status = 'almost cooked';
-      console.log(`The cookie is ${chocolateCookies.status}! Just a little more!`);
-    }
-    if (this.time < chocolateCookies.timeToCook - 5 && this.time>0){
-      chocolateCookies.status = 'raw';
-      console.log(`The cookie is ${chocolateCookies.status}! Continue cooking!`)
-    }
-    if (this.time >= chocolateCookies.timeToCook + 5){
-      chocolateCookies.status = 'burned';
-      console.log(`Oops! the cookie is ${chocolateCookies.status}! Better luck next time!`)
-      this.discardedCookies.push(chocolateCookies);
-      this.time=0;
-    }
+  putChocolateCookiesIn(){
+    var chocolateCookies = new ChocolateCookies();
+    this.inside.push(chocolateCookies);
   }
-  cookPeanutCookies(time){
-    this.time+=time
-    var peanutCookies = new PeanutCookies()
-    if (this.time >= peanutCookies.timeToCook && this.time < peanutCookies.timeToCook + 5){
-      peanutCookies.status = 'cooked';
-      console.log(`Peanut cookie is ${peanutCookies.status} beautifully!`)
-      this.cookedCookies.push(peanutCookies);
-      this.time=0;
-    }
-    if (this.time >= peanutCookies.timeToCook - 5 && this.time < peanutCookies.timeToCook){
-      peanutCookies.status = 'almost cooked';
-      console.log(`The cookie is ${peanutCookies.status}! Just a little more!`);
-    }
-    if (this.time < peanutCookies.timeToCook - 5 && this.time>0){
-      peanutCookies.status = 'raw';
-      console.log(`The cookie is ${peanutCookies.status}! Continue cooking!`)
-    }
-    if (this.time >= peanutCookies.timeToCook + 5){
-      peanutCookies.status = 'burned';
-      console.log(`Oops! the cookie is ${peanutCookies.status}! Better luck next time!`)
-      this.discardedCookies.push(peanutCookies);
-      this.time=0;
-    }
+  putPeanutCookiesIn(){
+    var peanutCookies = new PeanutCookies();
+    this.inside.push(peanutCookies);
   }
-  cookCheeseCookies(time){
-    this.time+=time
-    var cheeseCookies = new CheeseCookies()
-    if (this.time >= cheeseCookies.timeToCook && this.time < cheeseCookies.timeToCook + 5){
-      cheeseCookies.status = 'cooked';
-      console.log(`Cheese cookie is ${cheeseCookies.status} beautifully!`)
-      this.cookedCookies.push(cheeseCookies);
-      this.time=0;
-    }
-    if (this.time >= cheeseCookies.timeToCook - 5 && this.time < cheeseCookies.timeToCook){
-      cheeseCookies.status = 'almost cooked';
-      console.log(`The cookie is ${cheeseCookies.status}! Just a little more!`);
-    }
-    if (this.time < cheeseCookies.timeToCook - 5 && this.time>0){
-      cheeseCookies.status = 'raw';
-      console.log(`The cookie is ${cheeseCookies.status}! Continue cooking!`)
-    }
-    if (this.time >= cheeseCookies.timeToCook + 5){
-      cheeseCookies.status = 'burned';
-      console.log(`Oops! the cookie is ${cheeseCookies.status}! Better luck next time!`)
-      this.discardedCookies.push(cheeseCookies);
-      this.time=0;
+  putCheeseCookieIn(){
+    var cheeseCookies = new CheeseCookies();
+    this.inside.push(cheeseCookies);
+  }
+  cook(time){
+    for(let i=0; i<=time; i+=5){
+      for(let j=0; j<this.inside.length; j++){
+        if (i >= this.inside[j].timeToCook && i < this.inside[j].timeToCook + 5){
+          this.inside[j].status = 'cooked';
+        }
+        if (i >= this.inside[j].timeToCook - 5 && i < this.inside[j].timeToCook){
+          this.inside[j].status = 'almost cooked';
+        }
+        if (i < this.inside[j].timeToCook - 5 && i>0){
+          this.inside[j].status = 'raw';
+        }
+        if (i >= this.inside[j].timeToCook + 5){
+          this.inside[j].status = 'burned';
+        }
+        console.log(`${this.inside[j].type}, menit ke ${i} : ${this.inside[j].status}`)
+      }
     }
   }
 }
 
 var a = new Oven()
-a.cookChocolateCookie(10)
-a.cookChocolateCookie(5)
-a.cookChocolateCookie(5)
-a.cookChocolateCookie(25)
+a.putChocolateCookiesIn()
+a.putPeanutCookiesIn()
+a.putCheeseCookieIn()
+a.cook(35)
