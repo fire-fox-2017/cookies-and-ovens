a3977ff40173f73ca76e6db1731fe2c399e95548
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-04-07 11:24:54 +0700

    fixing code

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,67 @@
 //
 //
 // Your code here
+"use strict"
+class Oven {
+  constructor(cookies){
+    this.cookies = cookies;
+    this.timeProcess = 35; //menit maksimal untuk memasak
+  }
+
+  proses() {
+    let theCookies = this.cookies;
+    for (let i=5; i<this.timeProcess; i+=5){
+      if (i < theCookies.isHalfCooked){
+        console.log( `${theCookies.name}, on ${i}\'th minute is, ${theCookies.status[0]}`)
+      }else if (i == theCookies.isHalfCooked) {
+        console.log(`${theCookies.name}, on ${i}\'th minute is, ${theCookies.status[1]}`)
+      }else if (i == theCookies.isCooked){
+        console.log(`${theCookies.name}, on ${i}\'th minute is, ${theCookies.status[2]}`)
+      }else {
+        console.log(`${theCookies.name}, on ${i}\'th minute is, ${theCookies.status[3]}`)
+      }
+    }
+  }
+}
+
+class Cookies {
+  constructor (name){
+    this.name = name + ' cookies';
+    this.status = ['raw', 'half-cooked', 'cooked', 'over-cooked'];
+  }
+}
+
+class Peanut extends Cookies {
+  constructor(name){
+    super(name);
+    this.isHalfCooked = 10;
+    this.isCooked = 15;
+  }
+}
+
+class Chocolate extends Cookies {
+  constructor(name){
+    super(name);
+    this.isHalfCooked = 15;
+    this.cooked = 20;
+  }
+}
+
+class Cheese extends Cookies {
+  constructor(name){
+    super(name);
+    this.isHalfCooked = 10;
+    this.isCooked = 25;
+  }
+}
+
+let ovenPeanut = new Oven (new Peanut('peanut'));
+let ovenChocolate = new Oven (new Chocolate('chocolate'));
+let ovenCheese = new Oven (new Cheese('cheese'));
+
+console.log('====================================')
+ovenPeanut.proses()
+console.log('====================================')
+ovenChocolate.proses()
+console.log('====================================')
+ovenCheese.proses()
