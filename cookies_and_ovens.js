0ae2e6a99bbc86ed6dd3cc1e339e5b2233228789
arahito <uarahito@yahoo.com>
2017-04-07 16:18:08 +0700

    Cookies and Oven

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -22,97 +22,92 @@
 // Your code here
 
 class Cookie {
-  constructor() {
-    this._cooking_time = 0
-    this._status = ''
-    this._ripeTime = 0
-  }
-
-  cekStatus(time){
-    if (time < this._ripeTime-10) {
-      this._status = 'mentah'
-    } else if (time < this._ripeTime-5){
-      this._status = 'hampir matang'
-    } else if (time === this._ripeTime){
-      this._status = 'matang/selesai'
-    } else if (time > this._ripeTime){
-      this._status = 'hangus'
+    constructor() {
+        this._cooking_time = 0
+        this._status = ''
+        this._ripeTime = 0
     }
-    return this._status
-  }
+
+    cekStatus(time) {
+        if (time < this._ripeTime - 10) {
+            this._status = 'mentah'
+        } else if (time < this._ripeTime - 5) {
+            this._status = 'hampir matang'
+        } else if (time === this._ripeTime) {
+            this._status = 'matang/selesai'
+        } else if (time > this._ripeTime) {
+            this._status = 'hangus'
+        }
+        return this._status
+    }
+}
+
+class ChocolateCookie extends Cookie {
+    constructor() {
+        super()
+        this._time = 20
+        this._ripeTime = 15
+        this._name = "Kue Cokelat"
+    }
+
+    cooking_time() {
+        return super.cooking_time = this._time;
+    }
+
+    cekStatus(time) {
+        return super.cekStatus(time)
+    }
 
 }
 
-class ChocolateCookie extends Cookie{
-  constructor() {
-    super()
-    this._ripeTime = 15
-    this._name = "Kue Cokelat"
-  }
+class CheeseCookie extends Cookie {
+    constructor() {
+        super()
+        this._time = 30
+        this._ripeTime = 23
+        this._name = "Kue Keju"
+    }
 
-  cooking_time(){
-    return super.cooking_time = this._time;
-  }
-
-  cekStatus(time){
-    return super.cekStatus(time)
-  }
-
+    cooking_time() {
+        return super.cooking_time = this._time;
+    }
 }
 
-// let chocolateCookie = new ChocolateCookie()
-// console.log(chocolateCookie.cooking_time());
-// console.log(chocolateCookie.cekStatus());
-// console.log(chocolateCookie._ripeTime);
+class PeanutButterCookie extends Cookie {
+    constructor() {
+        super()
+        this._time = 30
+        this._ripeTime = 20
+        this._name = "Kue Kacang"
+    }
 
-class CheeseCookie extends Cookie{
-  constructor() {
-    super()
-    this._time = 30
-    this._ripeTime = 23
-    this._name = "Kue Keju"
-  }
-
-  cooking_time(){
-    return super.cooking_time = this._time;
-  }
-}
-
-class PeanutButterCookie extends Cookie{
-  constructor() {
-    super()
-    this._time = 30
-    this._ripeTime = 20
-    this._name = "Kue Kacang"
-  }
-
-  cooking_time(){
-    return super.cooking_time = this._time;
-  }
+    cooking_time() {
+        return super.cooking_time = this._time;
+    }
 }
 
 class Oven {
-  constructor() {
-    this._tempCookie = []
-    this._chocolate = new ChocolateCookie()
-    this._cheese = new CheeseCookie()
-    this._peanut = new PeanutButterCookie()
-  }
+    constructor() {
+        this._tempCookie = []
+        this._chocolate = new ChocolateCookie()
+        this._cheese = new CheeseCookie()
+        this._peanut = new PeanutButterCookie()
+    }
 
-  composition(){
-    this._tempCookie.push(this._chocolate, this._cheese, this._peanut)
-    // return this._tempCookie
-  }
+    composition() {
+        this._tempCookie.push(this._chocolate, this._cheese, this._peanut)
+        return this._tempCookie
+    }
 
-  bake(time){
-      for (let i = 5; i <= time; i+=5) {
-        for (let j = 0; j < this._tempCookie.length; j++) {
-          console.log(`${this._tempCookie[j]._name}, menit ke ${i} : ${this._tempCookie[j].cekStatus(i)} \n`);
+    bake(time) {
+        for (let i = 5; i <= time; i += 5) {
+            for (let j = 0; j < this._tempCookie.length; j++) {
+                console.log(`${this._tempCookie[j]._name}, menit ke ${i} : ${this._tempCookie[j].cekStatus(i)} \n`);
+            }
         }
-      }
-  }
+    }
 }
 
 let oven = new Oven()
-console.log(oven.composition());
-console.log(oven.bake(30));
+oven.composition()
+oven.bake(30)
